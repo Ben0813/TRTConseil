53b72da4150e61f32f79aea210a1248e3c7fc0b3
Ben0813 <benoitdewigne@gmail.com>
2023-10-28 14:28:46 +0200

    add SignupAdministrator and SignupConsultant components

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './components/views/Login';
 import SignupCandidate from './components/views/SignupCandidate';
 import SignupRecruiter from './components/views/SignupRecruiter';
+import SignupConsultant from './components/views/SignupConsultant';
+import SignupAdministrator from './components/views/SignupAdministrator';
 import ProfileCandidate from './components/views/ProfileCandidate';
 import ProfileRecruiter from './components/views/ProfileRecruiter';
 
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup-candidate" element={<SignupCandidate />} />
         <Route path="/signup-recruiter" element={<SignupRecruiter />} />
+        <Route path="/signup-consultant" element={<SignupConsultant />} />
+        <Route path="/signup-administrator" element={<SignupAdministrator />} />
         <Route path="/profile-candidate" element={<ProfileCandidate />} />
         <Route path="/profile-recruiter" element={<ProfileRecruiter />} />
       </Routes>
---
+++ front/src/components/views/SignupAdministrator.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const SignupAdministrator = () => {
+  const [name, setName] = useState('');
+  const [firstname, setFirstname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const url = 'http://localhost:3001/api/administrators';
+    try {
+      const response = await axios.post(url, {
+        name,
+        firstname,
+        email,
+        password
+      });
+      console.log('Compte administrateur créé avec succès:', response.data);
+    } catch (error) {
+      console.error('Erreur lors de la création du compte administrateur:', error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center bg-gray-900 min-h-screen text-white">
+      <div className="bg-gray-800 p-8 rounded-lg w-full md:w-1/3 my-8">
+        <form onSubmit={handleSubmit}>
+        <input
+            type="text"
+            placeholder="Nom"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="p-2 rounded bg-gray-700 w-full mb-4"
+          />
+          <input
+            type="text"
+            placeholder="Prénom"
+            value={firstname}
+            onChange={(e) => setFirstname(e.target.value)}
+            className="p-2 rounded bg-gray-700 w-full mb-4"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="p-2 rounded bg-gray-700 w-full mb-4"
+          />
+          <input
+            type="password"
+            placeholder="Mot de passe"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="p-2 rounded bg-gray-700 w-full mb-4"
+          />
+          <button type="submit" className="bg-green-500 p-2 rounded w-full">S'inscrire</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SignupAdministrator;
